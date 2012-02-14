a432b6d1f085096373b8c46b38c863fa15ee3213
Laurent Couvidou <lorancou@free.fr>
2012-02-14 18:37:20 +0100

    Polish

--- clean.sh
+++ clean.sh
@@ -5,16 +5,5 @@ rm -rf .gitignore~
 rm -rf *~
 rm -rf js/*~
 rm -rf js/#*#
-rm -rf js/dep/
-
-echo "Cleaning distrib/pokki..."
-rm -f distrib/pokki/minus.js
-rm -rf distrib/pokki/shader
-
-echo "Cleaning distrib/progfolio..."
-rm -f distrib/progfolio/.htaccess
-rm -f distrib/progfolio/minus.css
-rm -f distrib/progfolio/minus.html
-rm -f distrib/progfolio/minus.ico
-rm -f distrib/progfolio/minus.js
-rm -rf distrib/progfolio/shader
+rm -rf jsdep/*.js
+rm -rf build/
